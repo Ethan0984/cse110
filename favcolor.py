3ffb2b60dffd0ba228498107f6bc4a747da3903f
Ethan Stancliffe <sta22020@byui.edu>
2022-09-24 13:13:14 -0600

    Commit

---
+++ favcolor.py
@@ -0,0 +1,4 @@
+print ('Hello!')
+color = input('What is your favorite color: ')
+print ('So', color, 'is your favorite color, huh? To each their own I guess.')
+print('JK,', color ,'is a pretty cool color.')
