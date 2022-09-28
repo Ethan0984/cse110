4e595630fb22cb28b4c2ecc724e8cc65ff78d269
Ethan Stancliffe <sta22020@byui.edu>
2022-09-28 12:22:04 -0600

    com

--- mealpricecalc.py
+++ mealpricecalc.py
@@ -3,7 +3,15 @@ childmeal = float(input("What is the price of a child's meal? "))
 adultmeal = float(input("What is the price of an adult's meal? "))
 childcount = int(input("How many children are there? "))
 adultcount = int(input("How many adults are there? "))
-salestax = float(input("What is the sales tax rate? "))
+taxrate = float(input("What is the sales tax rate? "))
 subtotal = childmeal * childcount + adultmeal * adultcount
 print()
-print("Subtotal:", subtotal)
+print("Subtotal: $" + subtotal)
+salestax = taxrate * 0.01 * subtotal
+print("Sales Tax: $" + salestax)
+total = subtotal + salestax
+print("Total: $" + total)
+print()
+payment = float(input("What is the Payment Amount? "))
+change = payment - total
+print("Change: $" + change)
