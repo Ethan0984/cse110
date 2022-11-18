566bd7dc9a1a1276a7f7e4be449959951a45ca76
Ethan Stancliffe <sta22020@byui.edu>
2022-11-18 12:46:37 -0700

    chicken

--- cartpricing.py
+++ cartpricing.py
@@ -3,14 +3,15 @@ prices = []
 selection = None
 print("Welcome to the Shopping Cart Program!")
 print()
-while selection != "5":
+while selection != "6":
     print()
     print("Please select one of the following:")
     print("1. Add item")
     print("2. View cart")
     print("3. Remove item")
     print("4. Compute total")
-    print("5. Quit")
+    print("5. Compute average")
+    print("6. Exit")
     selection = input("Please enter an action: ")
 
     if selection == "1":
@@ -19,19 +20,25 @@ while selection != "5":
         newprice = float(input(f"What is the price of {newitem}? "))
         prices.append(newprice)
         print(f"'{newitem}' has been added to the cart.")
-    if selection == "2":
+    elif selection == "2":
         print("The contents of the cart are: ")
         for i in range(len(items)):
             item = items[i]
             price = prices[i]
             print(f"{i + 1}. {item} - ${price:.2f}")
-    if selection == "3":
+    elif selection == "3":
         for i in enumerate(items):
             index = int(input("Which item would you like to remove? ")) - 1
             user_choice = items[index]
             items.remove(user_choice)
             print("Item removed")
-    if selection == "4":
+    elif selection == "4":
         total = sum(prices)
-        print(f"Your total price is ${total}")
+        print(f"Your total price is ${total:.2f}")
+    elif selection == "5":
+        average = total/len(prices)
+        print(f"The average cost of your items is ${average:.2f}")
+    else:
+        if selection != "6":
+            print("Please enter a number 1-6")
 print("Thank you. Goodbye.")
